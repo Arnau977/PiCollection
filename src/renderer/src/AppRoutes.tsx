44adf13f6055419e70cbe8aa52b9b98e784c8780
Arnau977 <arnauete.19@gmail.com>
2025-01-16 23:51:06 +0100

    Test merging

--- src/renderer/src/AppRoutes.tsx
+++ src/renderer/src/AppRoutes.tsx
@@ -9,7 +9,7 @@ export default function AppRoutes(): JSX.Element {
         <Route path="/" element={<App />} />
         <Route path="/pictures" element={<>Pictures</>} />
         <Route path="/pictures/:id" element={<PicturePage />} />
-        <Route path="/easter-egg" element={<>Mis huevos</>} />
+        <Route path="/easter-egg" element={<>Mis huevos jeje</>} />
       </Routes>
     </Router>
   )
