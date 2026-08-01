de8d0eda450745042ed7a1923840e19d4f26b3f6
arnau977 <arnauete.19@gmail.com>
2026-08-01 22:44:17 +0200

    Add index on media.route

--- src/main/database/migrations/index.ts
+++ src/main/database/migrations/index.ts
@@ -3,10 +3,12 @@ import * as m0001InitialSchema from './0001_initial_schema'
 import * as m0002Series from './0002_series'
 import * as m0003MediaAiGenerated from './0003_media_ai_generated'
 import * as m0004MediaHash from './0004_media_hash'
+import * as m0005IndexMediaRoute from './0005_index_media_route'
 
 export const migrations: Record<string, Migration> = {
   '0001_initial_schema': m0001InitialSchema,
   '0002_series': m0002Series,
   '0003_media_ai_generated': m0003MediaAiGenerated,
-  '0004_media_hash': m0004MediaHash
+  '0004_media_hash': m0004MediaHash,
+  '0005_index_media_route': m0005IndexMediaRoute
 }
